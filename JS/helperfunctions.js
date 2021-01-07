485c85c4add3475940d25c9fab6efd26897e8da5
unknown <p.parkcontact@gmail.com>
2021-01-06 20:19:58 -0800

    testing application

--- JS/helperfunctions.js
+++ JS/helperfunctions.js
@@ -1,8 +1,4 @@
-require('dotenv').config();
 const tmi = require('tmi.js');
-// const { onMessageHandler } = require("../JS/helperfunctions.js")
-// const port = 8080;
-// Define configuration options
 const opts = {
   identity: {
     username: process.env.BOT_USERNAME,
@@ -10,13 +6,14 @@ const opts = {
   },
   channels: [process.env.CHANNEL_NAME]
 };
-// Create a client with our options
 const client = new tmi.client(opts);
-// Connect to Twitch:
-client.connect();
 
-// Called every time a message comes in
-const onMessageHandler = (target, context, msg, self) => {
+function rollDice () {
+  const sides = 6;
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+const onMessageHandler = function (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
   console.log(target);
   console.log(context.subscriber);
@@ -36,18 +33,7 @@ const onMessageHandler = (target, context, msg, self) => {
     console.log(`* Unknown command ${commandName}`);
   }
 }
-// Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
-}
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
-}
 
 
-// Register our event handlers (defined below)
-client.on('connected', onConnectedHandler);
-client.on('message', onMessageHandler);
 
+module.exports = { onMessageHandler };
