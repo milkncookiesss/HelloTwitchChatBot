e798907b5a95edd87b8bd05aeecaf217c7895b2a
unknown <p.parkcontact@gmail.com>
2021-01-19 18:40:12 -0800

    "added scripts, added start of TTT game, added sendLove functionality"

--- JS/helperfunctions.js
+++ JS/helperfunctions.js
@@ -1,3 +1,5 @@
+const { client } = require("tmi.js");
+
 function rollDice (x) {
   const sides = x;
   return Math.floor(Math.random() * x) + 1;
@@ -6,22 +8,73 @@ function rollDice (x) {
 const dice = (client, target, context, msg, self) => {
   const num = rollDice(6);
   client.say(target, `You rolled a ${num}`);
-  console.log(`* Executed ${commandName} command`);
 }
 
 const gamble = (client, target, context, msg, self) => {
   const num = rollDice(100);
-
-  if (context["display-name"] === 'kitty9o') {
-    client.say(target, `${context["display-name"]} here is your win, you baby. BabyRage`)
+  const { "display-name": displayName } = context;
+  const channelName = target.slice(1, target.length);
+  
+  if (displayName === 'kitty9o') {
+    client.say(target, `${displayName} here is your win, you baby. BabyRage`)
   } else if (num === 1) {
-    client.say(target, `${context["display-name"]} You rolled a ${num}, tough luck.`);
-    client.say(target, `/timeout ${context["display-name"]} 600`)
+    client.say(target, `${displayName} You rolled a ${num}!! dibzPog WINNER WINNER CHICKEN DINNER!! dibzPog`);
+    client.timeout(channelName, displayName, 600, `They won the gamble by rolling 1.`)
+    .then((data) => {
+      console.log(`the ban was successful`);
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+  } else if (num > 1 && num <= 29) {
+    client.say(target, `${displayName}, You rolled a ${num}, try again. KEKW`);
+    client.timeout(channelName, displayName, 1, `They did not win the roll.`)
+    .then((data) => {
+      console.log(`see ya`);
+    })
+    .catch(err => console.error(err))
+  } else if (num === 69) {
+    client.say(target, `${displayName} rolled a ${num}, milkncG nice.... fucking nice....`);
   } else {
-    client.say(target, `${context["display-name"]}, You rolled a ${num}, you pass this time.`);
+    client.say(target, `${displayName}, You rolled a ${num}, dibzHYPE you pass this time! dibzHYPE`);
+  }
+}
+
+const cheerMessage = (channel, userstate, message) => {
+  console.log(`${channel}
+    ${userstate}`);
+}
+
+const sendLove = (client, target, context, msg, self, listOfPeople) => {
+  const channelName = target.slice(1, target.length);
+  const hearts = {
+    "dibz": "dibzHeart",
+    "danielfenner": "fenLove",
+    "milkncookiesss": "milkncH"
+  };
+  const { "display-name": displayName, username } = context;
+  const { subscriber, "badge-info": badgeInfo } = context;
+  const timer = username.length * 150;
+  if (!listOfPeople.includes(username) && (subscriber || badgeInfo ) && (displayName !== "Nightbot" || displayName !== "Streamelements")) {
+    // setTimeout(() => {client.say(target, `${displayName} ${hearts[channelName]}`);}, timer);
+    console.log(`${displayName} ${hearts[channelName]} timer is : ${timer}`);
+    listOfPeople.push(username);
+  }
+}
+
+class TicTactoe {
+  constructor (client, target, context, msg, self) {
+    this.target = target;
+    this.client = client;
+    this.board = [[0,0,0],[0,0,0],[0,0,0]]
+  }
+
+  displayTable = () => {
+    this.client.say(this.target, `[${this.board[0]}]`);
+    this.client.say(this.target, `[${this.board[1]}]`);
+    this.client.say(this.target, `[${this.board[2]}]`);
   }
 }
 
 
-
-module.exports = { dice, gamble };
+module.exports = { dice, gamble, cheerMessage, TicTactoe, sendLove };
--- server/bot.js
+++ server/bot.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const tmi = require('tmi.js');
-const { dice, gamble } = require("../JS/helperfunctions.js")
+const { dice, gamble, cheerMessage, TicTactoe, sendLove } = require("../JS/helperfunctions.js")
 // const port = 8080;
 // Define configuration options
 const opts = {
@@ -8,28 +8,53 @@ const opts = {
     username: process.env.BOT_USERNAME,
     password: process.env.OAUTH_TOKEN
   },
-  channels: ['milkncookiesss', 'dibz']
+  channels: ['milkncookiesss', 'dibz', 'DanielFenner']
 };
+const listOfPeople = ["Cookie_Nova", "Nightbot", "StreamElements"];
 // Create a client with our options
 const client = new tmi.client(opts);
 // Connect to Twitch:
 client.connect();
 
-console.log('what is this ', client);
+
+
 // Called every time a message comes in
 const onMessageHandler = (target, context, msg, self) => {
+  const channelName = target.slice(1, target.length);
+  const { "badge-info": badgeInfo } = context;
+  console.log(`------->`, badgeInfo);
   if (self) { return; } // Ignore messages from the bot
-  // console.log(target);
-  console.log(context);
+  console.log(`[ ${channelName} ], ${context["display-name"]}: ${msg}`);
   // Remove whitespace from chat message
   const commandName = msg.trim();
-
+  if (commandName === `!ttt`) {
+    const game = new TicTactoe(client, target, context, msg, self);
+    game.displayTable();
+  }
   if (commandName === '!gamble') {
     gamble(client, target, context, msg, self);
   }
+  switch (channelName) {
+    case 'dibz':
+      // sendLove(client, target, context, msg, self, listOfPeople);
+      break;
+    case 'milkncookiesss':
+      // sendLove(client, target, context, msg, self, listOfPeople);
+      break;
+    // case 'danielfenner':
+    //   // const listOfPeople = [];
+    //   // console.log(`---------> `,listOfPeople);
+    //   break;
+  }
 
   if (commandName === "!emoteonly") {
-    client.say(target, `/emoteonly`);
+    client.emoteonly(channelName)
+    .then((data) => {
+      client.say(target, `Only Emotes, get trolled chat.`);
+    })
+    .catch((err) => {
+      console.err(err);
+    })
   }
 
   // If the command is known, let's execute it
@@ -49,6 +74,12 @@ function onConnectedHandler (addr, port) {
 
 
 // Register our event handlers (defined below)
+client.on('connected', (target) => {
+  if (target === '#dibz') {
+    console.log('we are on dibz channel');
+  }
+})
 client.on('connected', onConnectedHandler);
 client.on('message', onMessageHandler);
+client.on('cheer', cheerMessage);
 
